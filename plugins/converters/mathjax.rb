656ed75427a1e14f4355db9ea3ca1835046769bc
rguillome <public.guillaume.renaudin@gmail.com>
2017-04-26 14:05:28 +0200

    Ajout de l'article sur Breizhcamp 2017 Modification du converter markdown pour intégrer mathjax.js

---
+++ plugins/converters/mathjax.rb
@@ -0,0 +1,42 @@
+# place mathjax.rb in _plugins directory of your blog
+require 'redcarpet'
+class HTMLwithAlbino < Redcarpet::Render::HTML
+  def block_code(code, language)
+    if language == 'mathjax'
+      "<script type=\"math/tex; mode=display\">
+        #{code}
+      </script>"
+    else
+      "<pre><code class=\"#{language}\">#{code}</code></pre>"
+    end
+  end
+  
+  def codespan(code)
+    if code[0] == "$" && code[-1] == "$"
+      code.gsub!(/^\$/,'')
+      code.gsub!(/\$$/,'')
+      "<script type=\"math/tex\">#{code}</script>"
+    else
+      "<code>#{code}</code>"
+    end
+  end
+end
+
+class Ruhoh
+  module Converter
+    module Markdown
+
+      def self.extensions
+        ['.md', '.markdown']
+      end
+      
+      def self.convert(content)
+        markdown = Redcarpet::Markdown.new(HTMLwithAlbino.new(:with_toc_data => true), 
+        :autolink => true, 
+        :fenced_code_blocks => true
+        )
+        markdown.render(content)
+      end
+    end
+  end
+end
